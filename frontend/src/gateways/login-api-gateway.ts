819d5bdc973f252e23aec47f2af01b5764ca9060
DouglasVolcato <douglasvolcato@gmail.com>
2024-05-18 09:03:31 -0300

    chore: ensure login api gateway implements login api interface

--- frontend/src/gateways/login-api-gateway.ts
+++ frontend/src/gateways/login-api-gateway.ts
@@ -1,11 +1,12 @@
 import { ClientPostRequestSenderInterface } from "src/domain/abstract/adapters/client-post-request-sender-interface";
+import { LoginApiInterface } from "src/domain/abstract/gateways/login-api-interface";
 import { LoginDto } from "src/domain/abstract/dtos/login/login-dto";
 
-export class LoginApiGateway {
+export class LoginApiGateway implements LoginApiInterface {
   private readonly loginUrl: string;
   private readonly clientPostRequestSender: ClientPostRequestSenderInterface;
 
-  constructor(
+  public constructor(
     loginUrl: string,
     clientPostRequestSender: ClientPostRequestSenderInterface
   ) {
@@ -13,7 +14,7 @@ export class LoginApiGateway {
     this.clientPostRequestSender = clientPostRequestSender;
   }
 
-  async login(loginData: LoginDto): Promise<string | null> {
+  public async login(loginData: LoginDto): Promise<string | null> {
     const response = await this.clientPostRequestSender.post(
       this.loginUrl,
       loginData
