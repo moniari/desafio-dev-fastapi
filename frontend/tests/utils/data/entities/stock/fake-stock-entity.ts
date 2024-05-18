54625bdb364ab45c5c68a6fc1943c4b00848c03f
DouglasVolcato <douglasvolcato@gmail.com>
2024-05-18 10:35:05 -0300

    refactor: remove unused files

--- frontend/tests/utils/data/entities/stock/fake-stock-entity.ts
+++
@@ -1,6 +0,0 @@
-import { StockEntity } from "src/domain/abstract/entities/stock-entity";
-import { FakeData } from "tests/utils/data/fake-data";
-
-export const makeFakeStockEntity = (): StockEntity => ({
-  symbol: FakeData.word(),
-});
